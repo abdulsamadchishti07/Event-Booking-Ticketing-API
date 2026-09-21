bbd3be2fef70086e2643f569feedb2827e0cfe33
Abdul Samad <abdulsamadchishti07@gmail.com>
2026-09-21 19:19:36 +0500

    TDD: User Forbiden, Unthentication

--- tests/test_locations.py
+++ tests/test_locations.py
@@ -1,10 +1,11 @@
+from pydantic_settings.sources.providers import json
 import httpx
 import pytest
 from sqlalchemy.orm import Session
 
 from app import model
 
-
+# verified seller creates a venue successfully:
 async def test_location(
     client: httpx.AsyncClient,
     seller_user: model.User,
@@ -14,7 +15,7 @@ async def test_location(
     payload = {
         "city": "Bahawalpur",
         "country": "Pakistan",
-        "adress_line": "Star City"
+        "address_line": "Star City"
     }
 
     # is seller verified
@@ -22,9 +23,46 @@ async def test_location(
 
     # status and respose code
     assert response.status_code == 201
-    data = response.json()
+    data = response.json() # unauthenticated request
+    response = await client.post("/locations", json={
+        "city": "Islamabad",
+        "country": "Pakistan",
+        "address_line": "Star City"
+    })
+    
+
     assert data["city"] == "Bahawalpur"
     assert data["country"] == "Pakistan"
-    assert data["adress_line"] == "Star City"
+    assert data["address_line"] == "Star City"
     assert data["seller_id"] == seller_user.seller_profile.id
     assert "id" in data
+
+# Customer is Forbidden
+async def test_create_losation_customer_forbidded(
+    client: httpx.AsyncClient,
+    user_header: dict[str, str]
+):
+    payload = {
+        "city": "Bahawalpur",
+        "country": "Pakistan",
+        "address_line": "Star City",
+    }
+
+    response = await client.post("/locations", headers=user_header, json=payload)
+    
+    assert response.status_code == 403
+
+# Unauthenticated Guest is Rejected
+async def test_create_location_unauthenticated(
+    client: httpx.AsyncClient
+):
+    payload = {
+        "city": "Bahawalpur",
+        "country": "Pakistan",
+        "address_line": "Star City"    
+    }
+
+    # No header send
+    response = await client.post("/locations", json=payload)
+
+    assert response.status_code == 401
