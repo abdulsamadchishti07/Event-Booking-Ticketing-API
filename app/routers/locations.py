3fe7b30dbdce39c3a04f3b009667d91af72494f9
Abdul Samad <abdulsamadchishti07@gmail.com>
2026-09-21 22:26:07 +0500

    location by ID and all locations

--- app/routers/locations.py
+++ app/routers/locations.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from app.database import get_db, model, schema
-from app.routers.auth import oauth2
+from app.core import oauth2
 
 router = APIRouter(
     prefix="/locations",
@@ -21,7 +21,7 @@ def create_location(
     current_user: Annotated[model.User, Depends(oauth2.require_seller)],
     db: Session= Depends(get_db)
 ):
-    # check seller profile exist or not
+    # check seller profile exist or nots
     if not current_user.seller_profile:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
@@ -41,3 +41,33 @@ def create_location(
     db.refresh(new_location)
 
     return new_location
+
+@router.get(
+    "",
+    response_model=list[schema.LocationOut],
+    summary="Get all locations"
+)
+def get_locations(
+    db: Session = Depends(get_db)
+):
+    locations = db.query(model.Location).all()
+    return locations
+
+
+@router.get(
+
+    "/{id}",
+    response_model=schema.LocationOut,
+    summary="Get location by ID"
+)
+def get_location_by_id(
+    id: int,
+    db: Session = Depends(get_db)
+):
+    location = db.query(model.Location).filter(model.Location.id == id).first()
+    if not location:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Location with id {id} not found"
+        )
+    return location
